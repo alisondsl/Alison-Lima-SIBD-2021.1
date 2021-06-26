81ead980b6c1f569f8ccb9c83ea5dbf8ffd212d6
alisondsl <74928657+alisondsl@users.noreply.github.com>
2021-06-26 13:54:25 -0300

    TP-4

---
+++ TP-4/src/excecoes/Testando.java
@@ -0,0 +1,31 @@
+package excecoes;
+
+public class Testando {
+
+	
+	public static void main(String[] args) throws ExcecaoDivisaoResultadoZero {
+		/*Posicoes aA = new Posicoes();
+		Posicoes bB = new Posicoes();
+		
+		
+		aA.setArrayANaPosicao(20, 10); //demonstração com posição superior ao tamanho 10
+		bB.setArrayBNaPosicao(4, 3); //demonstração com array entre as posições 1 e 10
+		
+		
+		Posicoes cC = new Posicoes();
+		Posicoes dD = new Posicoes();
+		Posicoes eE = new Posicoes();
+		cC.calcula(1, 2); //resultado igual a 0
+		dD.calcula(10, 0); //divisão por 0
+		eE.calcula(2, 1); //divisão sem excepcion
+		*/
+		
+		
+		Posicoes over = new Posicoes();
+	
+		over.CalculaDivisaoArrays();
+		
+		
+		
+	}
+}
